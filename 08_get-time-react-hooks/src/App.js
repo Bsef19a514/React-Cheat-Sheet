9122b796d1a7b9f703f678c0c6816c49215d7395
zain sattar <zainsattar17@gmail.com>
2022-10-14 17:51:20 +0500

    react hooks, digital clock , lec#35

--- 08_get-time-react-hooks/src/App.js
+++ 08_get-time-react-hooks/src/App.js
@@ -4,11 +4,15 @@ import React , { useState } from "react";
 function App() {
   let newTime=new Date().toLocaleTimeString()
   let[time,setTime]=useState(newTime)
- 
+  
+
   const getTime=()=>{
     newTime=new Date().toLocaleTimeString()
     setTime(newTime)
   }
+
+  setInterval(getTime,1000)
+  
   return (
   <>
    <h1>{time}</h1>
