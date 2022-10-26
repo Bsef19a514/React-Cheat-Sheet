4fcf093c3ac8700e8d959584637b8c7cb0b9ab69
zain sattar <zainsattar17@gmail.com>
2022-10-26 23:13:23 +0500

    use bootstrap in react lec#50

--- 10_react-forms/src/Bootstrap.js
+++ 10_react-forms/src/Bootstrap.js
@@ -4,44 +4,41 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 const Bootstrap=()=>{
     return(
     <>
-    <h1>Bootstrap</h1>
-        <div class="input-group mb-3">
-  <span class="input-group-text" id="basic-addon1">@</span>
-  <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"/>
-</div>
-
-<div class="input-group mb-3">
-  <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
-  <span class="input-group-text" id="basic-addon2">@example.com</span>
-</div>
-
-<label for="basic-url" class="form-label">Your vanity URL</label>
-<div class="input-group mb-3">
-  <span class="input-group-text" id="basic-addon3">https://example.com/users/</span>
-  <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3"/>
-</div>
-
-<div class="input-group mb-3">
-  <span class="input-group-text">$</span>
-  <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)"/>
-  <span class="input-group-text">.00</span>
-</div>
-
-<div class="input-group mb-3">
-  <input type="text" class="form-control" placeholder="Username" aria-label="Username"/>
-  <span class="input-group-text">@</span>
-  <input type="text" class="form-control" placeholder="Server" aria-label="Server"/>
-</div>
-
-<div class="input-group">
-  <span class="input-group-text">With textarea</span>
-  <textarea class="form-control" aria-label="With textarea"></textarea>
-</div>
+        <h1 class="text-capitalize text-md-center m-1">Signup</h1>
+        <form >
+            <label >First Name:</label>
+            <input class="form-control" type="text" name="firstname" placeholder="Enter first name" />
+            <br/>
+            <br/>
+            <label>Last Name:</label>
+            <input class="form-control" type="text" name="lastname" placeholder="Enter lname name" />
+            <br/>
+            <br/>
+            <label>Email:</label>
+            <input class="form-control" type="text" name="email" placeholder="Enter your email" />
+            <br/>
+            <br/>
+            <label>Password:</label>
+            <input class="form-control" type="text" name="password" placeholder="Enter password" />
+            <br/>
+            <br/>
+            <label>Confirm Password:</label>
+            <input class="form-control" type="text" name="cPassword" placeholder="Confirm password" />
+            <br/>
+            <br/>
+            <div class="text-center">
+            <input type="submit" class="btn btn-outline-success " value="submit"/>
+            </div>
+            <br/>
+            <br/>
+        </form>
     </>
     )
 }
 
 // How to use bootstrap
 // 1) npm i bootstrap
-// 2) goto node_modules=>bootstarp=>dist=>css=>bootstrap.min.css
+// 2) import node_modules=>bootstarp=>dist=>css=>bootstrap.min.css in your js file
+// 3) intall IntelliSense for CSS class extansion in vs code
+// 4) install Bootstrap 4, Font awesome 4, extension in vs code
 export default Bootstrap
