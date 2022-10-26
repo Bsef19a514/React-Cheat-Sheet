e6cba5f9b70b124e281913cd068e7807feb3f846
zain sattar <zainsattar17@gmail.com>
2022-10-26 22:46:25 +0500

    use bootstrap in react lec#50

--- 10_react-forms/src/Incrementer.js
+++ 10_react-forms/src/Incrementer.js
@@ -1,5 +1,9 @@
 import React,{useState} from "react"
+import Button from '@mui/material/Button';
 import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+import Tooltip from '@mui/material/Tooltip';
+import "./App.css"
 
 const Incrementer=()=>{
     let number=0
@@ -27,9 +31,13 @@ const Incrementer=()=>{
         <div>
             <h1>{newNumber}</h1>
             {/* using font awesone icons */}
-            <button onClick={increment}><i class="fa-solid fa-plus"></i></button>
+            <Tooltip title="Increment">
+                <Button className="addButton" onClick={increment}><AddIcon/></Button>
+            </Tooltip>
             {/* using material ui icons */}
-            <button onClick={decrement}><AddIcon/></button>
+            <Tooltip title="Decrement">
+                <Button className="subtractButton" onClick={decrement}><RemoveIcon/></Button>
+            </Tooltip>
         </div>
     </>)
 }
--- 10_react-forms/src/index.js
+++ 10_react-forms/src/index.js
@@ -6,8 +6,10 @@ import LoginForm from './LoginForm';
 import LoginForm2 from './LoginForm2';
 import SignupForm from './SignupForm';
 import Incrementer from './Incrementer';
+import Bootstrap from './Bootstrap';
 
 const root=ReactDOM.createRoot(document.getElementById("root"))
 root.render(
- <Incrementer/>
+//  <Incrementer/>
+<Bootstrap/>
 )
