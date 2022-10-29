5ef37dd02887c58c6df386b8d6b3c82d01388073
zain sattar <zainsattar17@gmail.com>
2022-10-29 21:02:36 +0500

    useEffect hook | lec#57

--- 15_use-effect-hook/src/App.js
+++ 15_use-effect-hook/src/App.js
@@ -1,10 +1,21 @@
-import {useState} from "react"
+import {useState,useEffect} from "react"
 
 function App() {
-  let[number,setNumber]=useState()
+  let[number,setNumber]=useState(0)
+  const onClickEventHandler=()=>{
+    setNumber(number+1)
+    // alert(number) // first runs alert then update the value
+  }
+
+  // useEffect gets called everytime when App componet is rendered
+  // like page refresh and setNumber() is called
+  useEffect(()=>{
+    alert(number) 
+  })
+
   return (
     <div className="App">
-      <h1>number</h1>
+      <h1>{number}</h1>
       <button onClick={onClickEventHandler}>Click me</button>
     </div>
   );
